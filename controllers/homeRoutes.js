d9ab41ef9aa23e406aeceb1122803441edf3d31b
park-d <54589582+park-d@users.noreply.github.com>
2022-10-11 18:03:07 -0500

    fixed some issues with how the instructor files had naming

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const {Post, Comment, User} = require("../models");
+
+router.get("/", async (req, res) => {
+    const posts = await Post.findAll({
+        include: [User],
+    });
+    const allPosts = posts.map((post) => post.get({plain: true}));
+    res.render("all-posts", {allPosts});
+});
+
+module.exports = router;

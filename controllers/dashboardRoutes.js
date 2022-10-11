1598a59375d16d93cbd99e8572970e8253c1974e
park-d <54589582+park-d@users.noreply.github.com>
2022-10-11 18:58:13 -0500

    added in the route for dashboard page

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const {Post} = require("../models");
+
+router.get("/", async (req, res) => {
+    try {
+        const posts = await Post.findAll({
+            where: {
+                userId: req.session.userId
+            }
+        });
+        const allPosts = posts.map((post) => post.get({plain: true}));
+        res.render("all-posts-admin", {layout: "dashboard", allPosts});
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,11 +2,15 @@ const router = require("express").Router();
 const {Post, Comment, User} = require("../models");
 
 router.get("/", async (req, res) => {
-    const posts = await Post.findAll({
-        include: [User],
-    });
-    const allPosts = posts.map((post) => post.get({plain: true}));
-    res.render("all-posts", {allPosts});
+    try {
+        const posts = await Post.findAll({
+            include: [User],
+        });
+        const allPosts = posts.map((post) => post.get({plain: true}));
+        res.render("all-posts", {allPosts});
+    } catch(err) {
+        res.status(400).json(err);
+    }
 });
 
 module.exports = router;

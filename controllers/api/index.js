f1dd3dcab3e254167e904a0b4ba81488d9006541
park-d <54589582+park-d@users.noreply.github.com>
2022-10-11 20:22:30 -0500

    added in the new user, login, and logout routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes.js');
-const postRoutes = require('./postRoutes');
-const commentRoutes = require('./commentRoutes');
+// const postRoutes = require('./postRoutes.js');
+// const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+// router.use('/posts', postRoutes);
+// router.use('/comments', commentRoutes);
 
 module.exports = router;

50241f9c84d6e65dac00dd58a667bf280b562f8c
park-d <54589582+park-d@users.noreply.github.com>
2022-10-11 23:12:17 -0500

    added in all the dashboard routes, still need front end functionality

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,16 +1,34 @@
 const router = require("express").Router();
 const {Post} = require("../models");
-const withAuth = require("../utils/auth")
+const withAuth = require("../utils/auth");
 
 router.get("/", withAuth, async (req, res) => {
     try {
-        const posts = await Post.findAll({
+        const allPosts = await Post.findAll({
             where: {
                 userId: req.session.userId
             }
         });
-        const allPosts = posts.map((post) => post.get({plain: true}));
-        res.render("all-posts-admin", {layout: "dashboard", allPosts});
+        const posts = allPosts.map((post) => post.get({plain: true}));
+        res.render("all-posts-admin", {layout: "dashboard", posts});
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+router.get('/new', withAuth, (req, res) => {
+    res.render('new-post', {layout: 'dashboard'});
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+        const thePost = await Post.findByPk(req.params.id);
+
+        if(thePost) {
+            const post = thePost.get({plain: true});
+
+            res.render('edit-post', {layout: 'dashboard', post});
+        }
     } catch(err) {
         res.status(400).json(err);
     }
